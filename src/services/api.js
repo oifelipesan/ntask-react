cb8ac05ef160bf31d4f967bdb5c8e9e644a1d338
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-12-17 18:17:44 -0300

    baseUrl api

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  // baseURL: 'https://ntask-api.felipesan.com/'
-  baseURL: "http://192.168.1.4:3333"
+  baseURL: "https://ntask-api.felipesan.com/"
+  // baseURL: "http://192.168.1.4:3333"
 });
 
 api.interceptors.request.use(async config => {
